75f8cbd754a4f791d2b4e1d8f6aa3f7076b206a0
Gazua300 <franfidinena@gmail.com>
2021-08-04 15:49:28 -0300

    Implementaçõa dos botões para troca de tela

--- src/components/TelaDeContratar/TelaDeContratar.js
+++ src/components/TelaDeContratar/TelaDeContratar.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import Header from '../Header/Header'
+import './TelaDeContratar.css'
 
 
 
-export default function TelaDeContratar (){
-    return(
-<>
-<button input="type">Valor minimo</button>
-<button input="type"></button>
-    
-</>
-    )
-    
+export default class TelaDeContratar extends React.Component{
+
+    render(){
+         return(
+            <div>
+            <Header trocarTela={this.props.trocarTela}/>
+            
+            <div  class="conteudo">
+            <input type="text" placeholder="Valor mínimo"/>
+            <input type="text" placeholder="Valor máximo"/>
+            <input type="text" placeholder="Busca por título ou descrição"/>
+            <select class="sel">
+                <option value="">Sem ordenação</option>
+                <option value="">Menor valor</option>
+                <option value="">Maior valor</option>
+                <option value="">Título</option>
+                <option value="">Prazo</option>
+            </select>
+            </div>
+                
+            </div>
+        )
     }
+   
+    
+}
