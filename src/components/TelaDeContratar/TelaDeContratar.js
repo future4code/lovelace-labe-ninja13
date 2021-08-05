f1b7fe5e412028f296217ac46505a3813e946edc
matheus1810 <matheussouza1810@gmail.com>
2021-08-05 05:31:03 -0300

    fiz a função pra pegar os serviços

--- src/components/TelaDeContratar/TelaDeContratar.js
+++ src/components/TelaDeContratar/TelaDeContratar.js
@@ -1,10 +1,31 @@
+import Axios from 'axios'
 import React from 'react'
 import Header from '../Header/Header'
 import './TelaDeContratar.css'
 
-
-
 export default class TelaDeContratar extends React.Component{
+
+
+
+
+componentDidMount(){
+    
+    this.getAllJobs()
+}
+
+
+getAllJobs=()=>{
+    const url =`https://labeninjas.herokuapp.com/jobs`
+    const headers = {
+        headers: {
+            Authorization: "8f358eda-ff93-4ae6-9905-c06d326e4ad8"
+        }
+    }
+    Axios.get(url,headers)
+    .then((res)=>{console.log(res.data.jobs)})
+    .catch((err)=>{console.log(err)})
+}
+
 
     render(){
          return(
