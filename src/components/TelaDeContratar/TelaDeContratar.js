68864653203939cdec330a3ac3156175319e45fd
matheus1810 <matheussouza1810@gmail.com>
2021-08-05 07:12:51 -0300

    arrumei o input da data e o input dos pagamentos

--- src/components/TelaDeContratar/TelaDeContratar.js
+++ src/components/TelaDeContratar/TelaDeContratar.js
@@ -5,11 +5,13 @@ import './TelaDeContratar.css'
 
 export default class TelaDeContratar extends React.Component{
 
-
+state={
+   listaDeServicos:[]
+}
 
 
 componentDidMount(){
-    
+
     this.getAllJobs()
 }
 
@@ -22,16 +24,27 @@ getAllJobs=()=>{
         }
     }
     Axios.get(url,headers)
-    .then((res)=>{console.log(res.data.jobs)})
+    .then((res)=>{
+        this.setState({
+            listaDeServicos:res.data.jobs
+        })
+        console.log(res.data.jobs)
+       })
     .catch((err)=>{console.log(err)})
 }
 
 
     render(){
+    
+    const mostrar = this.state.listaDeServicos.map((item)=>{
+        return <li>
+            {item.title}
+              {item.dueDate} 
+                   </li>
+    })
          return(
             <div>
             <Header trocarTela={this.props.trocarTela}/>
-            
             <div  class="conteudo">
             <input type="text" placeholder="Valor mínimo"/>
             <input type="text" placeholder="Valor máximo"/>
@@ -43,6 +56,7 @@ getAllJobs=()=>{
                 <option value="">Título</option>
                 <option value="">Prazo</option>
             </select>
+            {mostrar}
             </div>
                 
             </div>
